bec58649ff53f41e34365561a128014cd7ce610a
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-26 20:23:58 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-<< "five"
+add_element_to_end_of_array.push ("five")
 end
-end
+
 
 def add_element_to_start_of_array(array, element)
 
